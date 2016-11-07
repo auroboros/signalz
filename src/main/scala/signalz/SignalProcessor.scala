080779b326246a105a723f12988f4a95e3c906d1
John McGill <jmcg.music@gmail.com>
2016-11-07 15:05:08 -0500

    random state idea, may have accessibility issues

--- src/main/scala/signalz/SignalProcessor.scala
+++ src/main/scala/signalz/SignalProcessor.scala
@@ -1,20 +1,33 @@
 package signalz
 
-import scala.concurrent.Future
-import scala.util.Random
-import scalaz.Alpha.{A, B}
-import scalaz.{Bifunctor, Functor}
+import scalaz._
+import Scalaz._
 
 /**
   * Created by johnmcgill on 10/25/16.
   */
 
-class SignalProcessor[A](signalOut: => A){
+case class SignalProcessor[A, B](process: A => B) {
 
-  def getSignalOut = signalOut
-}
+  // At this point does this even merit a wrapper or is this just simple function composition?
+  // Use scalaz Function1 functor or just use "andThen"?
 
-class SignalProcessorFunctor extends Functor[SignalProcessor] {
-  override def map[A, B](fa: SignalProcessor[A])(f: (A) => B): SignalProcessor[B] =
-    new SignalProcessor(f(fa.getSignalOut))
+  def map[C](postProcess: B => C) = SignalProcessor(process.map(postProcess))
+
+  // Monoid for B so we can get initial state & then mappend to thread signal through? (Default process implementation?)
+  def statefulMap[C](postProcess: B => C) = {
+
+//    val mb = implicitly[Monoid[B]]
+//    var storedState: B = mb.zero
+    var storedState: Option[B] = None
+
+    val instrumentedFunction = process map { transientB =>
+      storedState = Some(transientB)
+      storedState.get
+    } map postProcess
+
+    SignalProcessor(instrumentedFunction)
+  }
+
+  def flatMap[C](nextSignalProcessor: SignalProcessor[B, C]): SignalProcessor[A, C] = SignalProcessor(process.map(nextSignalProcessor.process))
 }
