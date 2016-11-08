b26696b3d8f0a38e90885f35a31c6ab3dd977f11
John McGill <jmcg.music@gmail.com>
2016-11-07 23:55:47 -0500

    notes on forking processor varieties

--- src/main/scala/signalz/MonoStateProcessor.scala
+++ src/main/scala/signalz/MonoStateProcessor.scala
@@ -17,3 +17,11 @@ case class MonoStateProcessor[I,S](processF: S => S, initState: S, preF: Option[
   val coreFunc: (I) => S = tmp andThen nextStateF
 //  val coreFunc: (I) => Any = postF.map(pf => tmp2 andThen pf).getOrElse(tmp2)
 }
+
+// For type parameters to work, would likely have to have several flavors of each processing style. How is this better than S => S (pre-curried/closure) functions
+// being given straight to the processor?
+case class MonoStatePreTransformProcessor[I,S]
+
+case class MonoStatePostTransformProcessor[S,O]
+
+case class MonoStatePrePostTransformProcessor[I,S,O]
