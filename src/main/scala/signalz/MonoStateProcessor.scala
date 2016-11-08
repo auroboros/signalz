00e384d56ba8628813acdaf95bb176d6da0bf924
John McGill <jmcg.music@gmail.com>
2016-11-07 22:33:23 -0500

    simplified with required initial state since half-monoid usage didn't really make sense (mappend was irrelevant)

--- src/main/scala/signalz/MonoStateProcessor.scala
+++ src/main/scala/signalz/MonoStateProcessor.scala
@@ -1,17 +1,12 @@
 package signalz
 
-import com.scalaudio.amp.immutable.ugen.OscState
-import com.scalaudio.core.types.PitchRichInt
-
-import scalaz._
-
 /**
   * Created by johnmcgill on 10/25/16.
   */
 
-case class MonoStateProcessor[A](process: A => A, modify: Option[A => A] = None, initState: Option[A] = None)(implicit aMonoid: Monoid[A]) {
+case class MonoStateProcessor[A](process: A => A, initState: A, modify: Option[A => A] = None) {
 
-  var state : A = initState.getOrElse(aMonoid.zero)
+  var state : A = initState
 
   val coreFunc = modify.map(f => f.andThen(process)).getOrElse(process)
 
@@ -19,9 +14,4 @@ case class MonoStateProcessor[A](process: A => A, modify: Option[A => A] = None,
     state = coreFunc(state)
     state
   }
-}
-
-object OscStateMonoid extends Monoid[OscState] {
-  override def append(a: OscState, b: => OscState): OscState = b
-  override def zero: OscState = OscState(0, PitchRichInt(440).Hz, 0)
 }
--- src/test/scala/signalz/MonoStateProcessorSpec.scala
+++ src/test/scala/signalz/MonoStateProcessorSpec.scala
@@ -1,27 +1,35 @@
 package signalz
 
 import com.scalaudio.amp.immutable.ugen.{OscState, SineStateGen}
-import com.scalaudio.core.AudioContext
+import com.scalaudio.core.{AudioContext, CoreSyntax, ScalaudioConfig}
+import com.scalaudio.core.engine.samplewise.AmpOutput
 import com.scalaudio.core.types.PitchRichInt
 import org.scalatest.{FlatSpec, Matchers}
 
 import scalaz._
 import Scalaz._
-
+import scala.concurrent.duration._
 /**
   * Created by johnmcgill on 11/7/16.
   */
-class MonoStateProcessorSpec extends FlatSpec with Matchers {
+class MonoStateProcessorSpec extends FlatSpec with Matchers with CoreSyntax {
 
-  implicit val oscStateMonoid = OscStateMonoid
+  implicit val audioContext = AudioContext(ScalaudioConfig(nOutChannels = 1))
+  val defaultOscState = OscState(0, PitchRichInt(440).Hz, 0)
 
   "signalz" should "construct a stateful synth" in {
-    implicit val audioContext = AudioContext()
 
-    val msp = MonoStateProcessor(SineStateGen.nextState)
+    val msp = MonoStateProcessor(SineStateGen.nextState, defaultOscState)
 
     1 to 1000 foreach {_ =>
       println(msp.nextState)
     }
+  }
+
+  "signalz" should "play audio from a stateful synth" in {
+
+    val msp = MonoStateProcessor(SineStateGen.nextState, defaultOscState)
+
+    AmpOutput(() => Array(msp.nextState.sample)).play(5.seconds)
   }
 }
