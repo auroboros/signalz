d49a178be1d0d8bb4df4626b5187b588cb2a914f
John McGill <jmcg.music@gmail.com>
2016-12-06 22:21:12 -0500

    inserted hooks for post-processing, but chances are.... this is wrong way

--- src/main/scala/signalz/SignalProcessingGraph.scala
+++ src/main/scala/signalz/SignalProcessingGraph.scala
@@ -3,9 +3,12 @@ package signalz
 /**
   * Created by johnmcgill on 12/6/16.
   */
-
+// TODO: Maybe instead of post-processing, take out counter completely?
+// Can have frame increment func be auto-added before every graph (map function on graph to make it clear?)
 trait SignalProcessingGraph {
   var currentFrame: Long = 0 // Current time in samples
+
+  def postFrameProcessingAction(): Unit = {}
 
   def run(nFrames: Long)
   def runWhile(loopCondition: Unit => Boolean)
@@ -17,12 +20,14 @@ class FunctionGraph(val signalGraph: () => _) extends SignalProcessingGraph {
     1L to nFrames foreach { s =>
       signalGraph()
       currentFrame = s
+      postFrameProcessingAction()
     }
 
   def runWhile(loopCondition: Unit => Boolean) =
     while (loopCondition()) {
       signalGraph()
       currentFrame += 1
+      postFrameProcessingAction()
     }
 
 }
@@ -30,8 +35,14 @@ class FunctionGraph(val signalGraph: () => _) extends SignalProcessingGraph {
 class StreamGraph(stream: => Stream[_]) extends SignalProcessingGraph {
 
   def run(nFrames: Long) =
-    stream.map(_ => currentFrame += 1).take(nFrames.toInt)
+    stream.map{_ =>
+      currentFrame += 1
+      postFrameProcessingAction()
+    }.take(nFrames.toInt)
 
   def runWhile(loopCondition: Unit => Boolean): Unit =
-    stream.map(_ => currentFrame += 1).takeWhile(loopCondition)
+    stream.map{_ =>
+      currentFrame += 1
+      postFrameProcessingAction()
+    }.takeWhile(loopCondition)
 }
